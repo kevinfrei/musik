7ac9e80c23dc102b354acb80d17bbb24fa9afa6d
Kevin Frei <kevinfrei@hotmail.com>
2023-05-31 23:03:51 -0700

    Formatting. Calling it a night

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,15 +4,15 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+  format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 // Look here: https://github.com/tauri-apps/tauri/issues/3543
 // And maybe here: https://medium.com/@marm.nakamura/practice-rust-and-tauri-make-an-image-viewer-4-39623547b06d
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        // .register_uri_scheme_protocol(uri_scheme, protocol)
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![greet])
+    // .register_uri_scheme_protocol(uri_scheme, protocol)
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
