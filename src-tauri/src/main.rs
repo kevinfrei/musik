96e6797c34bfaf459b96a71f7987d821f7f48720
Kevin Frei <kevinfrei@hotmail.com>
2023-05-31 22:47:16 -0700

    Formatted and commented...

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,9 +7,12 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+// Look here: https://github.com/tauri-apps/tauri/issues/3543
+// And maybe here: https://medium.com/@marm.nakamura/practice-rust-and-tauri-make-an-image-viewer-4-39623547b06d
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        // .register_uri_scheme_protocol(uri_scheme, protocol)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
